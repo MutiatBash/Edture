5e18dd8191d19457133ea6120104e1b6e8c0f4e8
Mutiat Bashua <mutiatbashua@gmail.com>
2024-08-08 23:16:43 +0100

    cleanup

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,14 +9,14 @@ const Profile = () => {
 	const { user, firstName, lastName, emailAddress } = useContext(userContext);
 	const role = user?.role;
 	const initials =
-		firstName && lastName ? `${firstName[0]}${lastName[0]}` : "ED";
+		firstName && lastName ? `${firstName[0]}${lastName[0]}` : "";
 
 	const studentContent = (
 		<>
 			<div className="flex flex-col gap-8">
 				<h3 className="text-2xl font-semibold">Personal Details</h3>
 				<div className="flex justify-start gap-12">
-					<div className="bg-primaryBlue text-white text-6xl font-trap-grotesk font-semibold flex flex-grow-0 justify-center items-center text-center w-64 max-w-80 h-64 rounded-full">
+					<div className="bg-primaryBlue text-white text-6xl font-trap-grotesk font-semibold flex flex-grow-0 justify-center items-center text-center w-64 max-w-80 h-64 rounded-full uppercase">
 						{initials}
 					</div>
 					<div className="flex flex-col gap-3 border border-lightGray rounded-lg p-6 text-darkGray w-96">
@@ -44,28 +44,28 @@ const Profile = () => {
 
 	const tutorContent = (
 		<>
-			<div className="font-trap-grotesk">
-				<h3 className="text-xl font-trap-grotesk">Personal Details</h3>
-				<div>
-					<div className="bg-primaryBlue p-2 text-center w-12 h-12">
+			<div className="flex flex-col gap-8">
+				<h3 className="text-2xl font-semibold">Personal Details</h3>
+				<div className="flex justify-start gap-12">
+					<div className="bg-primaryBlue text-white text-6xl font-trap-grotesk font-semibold flex flex-grow-0 justify-center items-center text-center w-64 max-w-80 h-64 rounded-full uppercase">
 						{initials}
 					</div>
-					<div className="flex flex-col gap-3 border border-lightGray rounded-lg p-4 text-darkGray">
+					<div className="flex flex-col gap-3 border border-lightGray rounded-lg p-6 text-darkGray w-96">
 						<div>
-							<h3>First Name</h3>
-							<p>{firstName}</p>
+							<h3 className="font-medium">First Name</h3>
+							<p className="text-lightGray">{firstName}</p>
 						</div>
 						<div>
-							<h3>Last Name</h3>
-							<p>{lastName}</p>
+							<h3 className="font-medium">Last Name</h3>
+							<p className="text-lightGray">{lastName}</p>
 						</div>
 						<div>
-							<h3>User Name</h3>
-							<p>{user?.username}</p>
+							<h3 className="font-medium">User Name</h3>
+							<p className="text-lightGray">{user?.username}</p>
 						</div>
 						<div>
-							<h3>Email Address</h3>
-							<p>{emailAddress}</p>
+							<h3 className="font-medium">Email Address</h3>
+							<p className="text-lightGray">{emailAddress}</p>
 						</div>
 					</div>
 				</div>
